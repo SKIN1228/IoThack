5941929a1421c9b252e9d33aaea5d556bd6286d5
TajiAki <s1620651@u.tsukuba.ac.jp>
2016-08-06 13:07:56 +0900

    Javaのクライアントが2人に対応しました

--- TCPClient(Java)/src/TcpClientTest.java
+++ TCPClient(Java)/src/TcpClientTest.java
@@ -7,33 +7,46 @@ import java.net.UnknownHostException;
 
 public class TcpClientTest {
 
-	static final String HOST = "192.168.11.2";
+	static final String HOST1 = "192.168.11.2";  //tajima
+	static final String HOST2 = "192.168.11.6";  //sakaino
 	static final int PORT = 10000;
 
 	/**
      * @param args
      */
     public static void main(String[] args) {
-        Socket socket = null;
+        Socket socket1 = null;
+        Socket socket2 = null;
         try {
-            socket = new Socket(HOST,PORT);
-            PrintWriter pw = new PrintWriter(socket.getOutputStream(),true);
+            socket1 = new Socket(HOST1,PORT);
+            socket2 = new Socket(HOST2,PORT);
+            PrintWriter pw1 = new PrintWriter(socket1.getOutputStream(),true);
+            PrintWriter pw2 = new PrintWriter(socket2.getOutputStream(),true);
             while(true){
             	System.out.println("please input ...");
             	InputStreamReader isr = new InputStreamReader(System.in);
             	BufferedReader br = new BufferedReader(isr);
             	String buf = br.readLine();
-            	pw.println(buf);
+            	pw1.println(buf);
+            	pw2.println(buf);
             }
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if( socket != null){
+        if( socket1 != null){
             try {
-                socket.close();
-                socket = null;
+                socket1.close();
+                socket1 = null;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if( socket2 != null){
+            try {
+                socket2.close();
+                socket2 = null;
             } catch (IOException e) {
                 e.printStackTrace();
             }
